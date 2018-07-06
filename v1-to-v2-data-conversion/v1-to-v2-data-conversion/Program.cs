2da9566b7bf3b29cc795b268218af3b75bbf5fe3
matt.roberts <matt.roberts@aflglobal.com>
2018-07-06 11:30:00 -0400

    DROP/CREATE Db and Convert Locations from V1 to V2

--- v1-to-v2-data-conversion/v1-to-v2-data-conversion/Program.cs
+++ v1-to-v2-data-conversion/v1-to-v2-data-conversion/Program.cs
@@ -10,33 +10,93 @@ namespace v1_to_v2_data_conversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Conversion.Data.v1.communityContext _ctxV1 = new Conversion.Data.v1.communityContext();
+            Conversion.Data.v2.CommunityV2Context _ctxV2 = new Conversion.Data.v2.CommunityV2Context();
 
-            Conversion.Data.v1.communityContext _ctx = new Conversion.Data.v1.communityContext();
+            Console.Clear();
+            Console.WriteLine("AFL Global Community Hub Data Conversion Tool v0.1");
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Press y/Y to begin (this will DROP and CREATE v2 database): ");
+            string _prompt = Console.ReadLine();
 
-            var employees = _ctx.Employees;
-            int idx = 1;
-
-            List<Conversion.Data.v1.EmployeeWork> employee_work = _ctx.EmployeeWork.ToList();
-
-            foreach(var employee in employees)
+            if (_prompt.ToUpper() != "Y")
             {
-                Console.WriteLine(idx + ": " + employee.EmpLast);
-                idx++;
+                Console.WriteLine("Exiting...");
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write("DROPPING Database...");
+                _ctxV2.Database.EnsureDeleted();
+                Console.WriteLine("success!");
 
-                try
+                Console.Write("CREATING Database...");
+                _ctxV2.Database.EnsureCreated();
+                Console.WriteLine("success!");
+
+
+                #region LOCATION
+                Console.Clear();
+
+                Console.WriteLine("Converting Locations");
+                Console.WriteLine("====================");
+
+                int _locConversionCount = 0;
+
+                foreach (Conversion.Data.v1.Locations v1Location in _ctxV1.Locations)
                 {
-                    List<Conversion.Data.v1.EmployeeWork> employeework = _ctx.EmployeeWork.Where(x => x.EmployeeId == employee.EmployeeId).ToList();
+                    Console.Write(String.Format("Converting Location: {0}...", v1Location.LocName));
 
-                    if (employeework.Count > 0)
+                    Conversion.Data.v2.Location v2Location = new Conversion.Data.v2.Location
                     {
-                        Console.WriteLine("Found Some work!");
+                        LocationCountry = v1Location.LocCountry,
+                        LocationName = v1Location.LocName
+                    };
+
+                    _ctxV2.Location.Add(v2Location);
+
+                    try
+                    {
+                        _ctxV2.SaveChanges();
+                        Console.WriteLine("success!");
+                        _locConversionCount++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("failed!");
                     }
                 }
-                catch (Exception ex)
-                {
-                    
-                }
+
+                Console.WriteLine(String.Format("Converted {0} of {1} Locations", _locConversionCount.ToString(), _ctxV1.Locations.Count().ToString()));
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+
+                #endregion
+
+                //var employees = _ctx.Employees;
+                //int idx = 1;
+
+                //List<Conversion.Data.v1.EmployeeWork> employee_work = _ctx.EmployeeWork.ToList();
+
+                //foreach(var employee in employees)
+                //{
+                //    Console.WriteLine(idx + ": " + employee.EmpLast);
+                //    idx++;
+
+                //    try
+                //    {
+                //        List<Conversion.Data.v1.EmployeeWork> employeework = _ctx.EmployeeWork.Where(x => x.EmployeeId == employee.EmployeeId).ToList();
+
+                //        if (employeework.Count > 0)
+                //        {
+                //            Console.WriteLine("Found Some work!");
+                //        }
+                //    }
+                //    catch (Exception ex)
+                //    {
+
+                //    }
+                //}
             }
 
             Console.ReadLine();
